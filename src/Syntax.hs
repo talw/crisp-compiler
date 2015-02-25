65f5b5d33308206b10fc9e79424b6cfa675dbc1b
Tal Walter <talw10@gmail.com>
2015-02-25 10:10:22 +0200

    Added a function to find out the free variables in a lambda expression

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -1,5 +1,9 @@
 module Syntax where
 
+import Data.Set (union)
+import qualified Data.Set as S
+import Data.Foldable (foldl')
+
 type Name = String
 
 data BinOp
@@ -57,3 +61,14 @@ data Expr
   {-| Extern Name [Name]-}
   | BinOpExp BinOp Expr Expr
   deriving (Eq, Ord, Show)
+
+findFreeVars :: Expr -> [Name]
+findFreeVars (FuncExp vars exp) = S.toList $ go vars exp
+ where
+  go vars (VarExp var) = if var `elem` vars then S.empty else S.singleton var
+  go vars (BinOpExp _ e1 e2) = go vars e1 `union` go vars e2
+  go vars (IfExp e1 e2 e3) = go vars e1 `union` go vars e2 `union` go vars e3
+  go vars (CallExp e1 es) = go vars e1 `union` theRest
+    where theRest = foldl' union S.empty $ map (go vars) es
+  go _ _ = S.empty
+findFreeVars _ = error "findFreeVars - not called with a function expression"
